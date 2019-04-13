a16a52c687aa9bd1cec7a2a144a35c646c4054ed
Udin <udines@users.noreply.github.com>
2019-04-13 20:21:53 +0700

    Add mock data for barang.

--- lib/data/barang_data.dart
+++ lib/data/barang_data.dart
@@ -4,4 +4,27 @@ class Barang {
   String type;
   int value;
   int weight;
+
+  Barang({
+    this.id,
+    this.name,
+    this.type,
+    this.value,
+    this.weight
+  });
+}
+
+abstract class BarangRepository {
+  Future<List<Barang>> fetchBarangs();
+}
+
+class FetchDataException implements Exception {
+  final _message;
+
+  FetchDataException([this._message]);
+
+  String toString() {
+    if (_message == null) return "Exception";
+    return "Exception: $_message";
+  }
 }
---
+++ lib/data/barang_data_prod.dart
@@ -0,0 +1,9 @@
+import 'barang_data.dart';
+
+class ProdBarangRepository implements BarangRepository {
+  @override
+  Future<List<Barang>> fetchBarangs() {
+    // TODO: implement fetchBarangs
+    return null;
+  }
+}
