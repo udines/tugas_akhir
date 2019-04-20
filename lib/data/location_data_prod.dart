fac862cb5af61455e8de35ed6e7fbbfebf81a671
Udin <udines@users.noreply.github.com>
2019-04-20 14:17:37 +0700

    Change desired accuracy from best to high

--- lib/data/location_data_prod.dart
+++ lib/data/location_data_prod.dart
@@ -6,7 +6,7 @@ import 'location_data.dart';
 class ProdLocationRepository implements LocationRepository {
   @override
   Future<LatLng> getCurrentLocation() async {
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     return LatLng(latitude: position.latitude, longitude: position.longitude);
   }
 
