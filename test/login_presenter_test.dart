6e71d0d3cbdac0b1aafa95079607605507acdb9d
Fata A <sendtofata@live.com>
2019-05-23 06:55:05 +0700

    Add test classes for repositories

--- test/login_presenter_test.dart
+++ test/login_presenter_test.dart
@@ -8,39 +8,46 @@ class LoginMock extends Mock {
 }
 
 void main() {
+  group('validate password', () {
+    final LoginMock loginMock = LoginMock();
 
-  test('password less than 6 characters', () {
-    var result = LoginMock().presenter.validatePassword("abc");
-    expect(result, false);
+    test('password less than 6 characters', () {
+      var result = loginMock.presenter.validatePassword("abc");
+      expect(result, false);
+    });
+
+    test('password is 6 characters', () {
+      var result = loginMock.presenter.validatePassword("abcdef");
+      expect(result, true);
+    });
+
+    test('password more than 6 characters', () {
+      var result = loginMock.presenter.validatePassword("abcdefghijkl");
+      expect(result, true);
+    });
   });
 
-  test('password is 6 characters', () {
-    var result = LoginMock().presenter.validatePassword("abcdef");
-    expect(result, true);
-  });
+  group('validate email', () {
+    final LoginMock loginMock = LoginMock();
 
-  test('password more than 6 characters', () {
-    var result = LoginMock().presenter.validatePassword("abcdefghijkl");
-    expect(result, true);
-  });
+    test('email has no .com', () {
+      var result = loginMock.presenter.validateEmail("user@email");
+      expect(result, false);
+    });
 
-  test('email has no .com', () {
-    var result = LoginMock().presenter.validateEmail("user@email");
-    expect(result, false);
-  });
+    test('email has no @', () {
+      var result = loginMock.presenter.validateEmail("useremail.com");
+      expect(result, false);
+    });
 
-  test('email has no @', () {
-    var result = LoginMock().presenter.validateEmail("useremail.com");
-    expect(result, false);
-  });
+    test ('email has no domain name', () {
+      var result = loginMock.presenter.validateEmail("user@.com");
+      expect(result, false);
+    });
 
-  test ('email has no domain name', () {
-    var result = LoginMock().presenter.validateEmail("user@.com");
-    expect(result, false);
-  });
-
-  test('email valid', () {
-    var result = LoginMock().presenter.validateEmail("user@email.com");
-    expect(result, true);
+    test('email valid', () {
+      var result = loginMock.presenter.validateEmail("user@email.com");
+      expect(result, true);
+    });
   });
 }
