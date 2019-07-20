d244d3d45cce2dc909403e884da4d0673b386c07
FATA <sendtofata@live.com>
2019-07-20 22:40:12 +0700

    Update location repository

--- lib/data/location/location_data.dart
+++ lib/data/location/location_data.dart
@@ -18,14 +18,3 @@ abstract class LocationRepository {
   Future<String> getAddress(double latitude, double longitude);
   Future<String> getCity();
 }
-
-class FetchDataException implements Exception {
-  final _message;
-
-  FetchDataException([this._message]);
-
-  String toString() {
-    if (_message == null) return "Exception";
-    return "Exception: $_message";
-  }
-}
