c34881afd39fe7326bf64811e59b0fe0debf7b9b
FATA <sendtofata@live.com>
2019-07-21 01:20:24 +0700

    Create shared preference util

---
+++ lib/utils/shared_preferences.dart
@@ -0,0 +1,50 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPref {
+
+  static const String KEY_USER_ID = 'preference_user_id';
+  static const String KEY_USER_NAME = 'preference_user_name';
+  static const String KEY_USER_ADDRESS = 'preference_user_address';
+  static const String KEY_USER_PHONE = 'preference_user_phone';
+  static const String KEY_USER_ADMIN = 'preference_user_admin';
+  
+  Future<bool> saveString(String key, String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(key, value);
+  }
+
+  Future<String> getString(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+
+  Future<bool> saveInt(String key, int value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setInt(key, value);
+  }
+
+  Future<int> getInt(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(key);
+  }
+
+  Future<bool> saveDouble(String key, double value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setDouble(key, value);
+  }
+
+  Future<double> getDouble(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getDouble(key);
+  }
+
+  Future<bool> saveBool(String key, bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setBool(key, value);
+  }
+
+  Future<bool> getBool(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(key);
+  }
+}
