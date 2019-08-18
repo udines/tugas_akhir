b9072db80b93d88b5080d37ee47ccbcc318504a9
FATA <fata@qontak.com>
2019-08-18 17:26:01 +0700

    Add logout to agent home presenter

--- lib/presenter/agent/home_presenter.dart
+++ lib/presenter/agent/home_presenter.dart
@@ -1,12 +1,14 @@
 import 'package:tugas_akhir/data/pickup/pickup_data.dart';
 import 'package:tugas_akhir/data/user/user_data.dart';
 import 'package:tugas_akhir/dependency_injection.dart';
+import 'package:tugas_akhir/utils/shared_preferences.dart';
 
 abstract class HomeViewContract {
   void onLoadPickupTransactionComplete(List<Pickup> pickups);
   void onLoadPickupTransactionError();
   void onGetCurrentUserComplete(User user);
   void onGetCurrentUserError();
+  void onLogoutSuccess();
 }
 
 class HomePresenter {
@@ -19,15 +21,31 @@ class HomePresenter {
     _userRepo = Injector().userRepository;
   }
 
-  void loadPickupTransactions(String agentId) {
-    _repository.fetchPickupsByUser(agentId)
-        .then((pickups) => _view.onLoadPickupTransactionComplete(pickups))
-        .catchError((onError) => _view.onLoadPickupTransactionError());
+  void logoutUser() {
+    _userRepo.logoutUser().then((onValue) {
+      _view.onLogoutSuccess();
+    });
   }
 
-  void getCurrentUser() {
-    _userRepo.fetchCurrentUser()
-        .then((user) => _view.onGetCurrentUserComplete(user))
-        .catchError((onError) => _view.onGetCurrentUserError());
+  void clearPreferences() {
+    SharedPref().clearData();
+  }
+
+  void loadPickupTransactions(String agentId) async {
+    try {
+      final pickups = await _repository.fetchPickupsByAgent(agentId);
+      _view.onLoadPickupTransactionComplete(pickups);
+    } catch(e) {
+      _view.onLoadPickupTransactionError();
+    }
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _userRepo.fetchCurrentUser();
+      _view.onGetCurrentUserComplete(user);
+    } catch(e) {
+      _view.onGetCurrentUserError();
+    }
   }
 }
